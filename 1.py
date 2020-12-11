6857198c122438a3197034da85a33bcf2f0e4f79
andrew-wl <the_3lementalist@hotmail.com>
2020-12-11 15:43:28 +1100

    1b Finished

--- 1.py
+++ 1.py
@@ -11,10 +11,27 @@ def main(f):
 		B +=1
 		if B == len(n):
 			A += 1
-			B = A + 1
+			B = A+1
 		if n[A] + n[B] == 2020:
 			print(n[A]*n[B])
 			break
+	# what is the product of the three entries that sum to 2020?
+	A = 0
+	B = 1
+	C = 2
+	while True:
+		if n[A] + n[B] + n[C] == 2020:
+			print(n[A]*n[B]*n[C])
+			break
+		C += 1
+		if C == len(n):
+			B += 1
+			C = B+1
+		if B == len(n)-1:
+			A += 1
+			B = A+1
+			C = B+1
+
 
 if __name__ == '__main__':
 	main('input.txt')
